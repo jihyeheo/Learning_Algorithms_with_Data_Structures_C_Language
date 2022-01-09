b5571ca9cbc3866617ac7ef87678250150480c8e
jihye_heo <64202709+jihyeheo@users.noreply.github.com>
2022-01-09 19:11:28 +0900

    20220109

---
+++ BASIC/basic1-2(1008).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("%.9lf", (double)a / (double)b);
+    return 0;
+}
