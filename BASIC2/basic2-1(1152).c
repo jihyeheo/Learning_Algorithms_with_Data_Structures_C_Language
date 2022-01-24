1b995e854844137797745120169e0674642abfa2
jihye_heo <64202709+jihyeheo@users.noreply.github.com>
2022-01-24 16:35:25 +0900

    20220124

---
+++ BASIC2/basic2-1(1152).c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int n = 1;
+    char str[1000001];
+    scanf("%[^\n]s", str);
+    for (int i = 0; i < strlen(str); i++)
+        if (str[i] == ' ') n++;
+    if (str[0] == ' ' && n) n--;
+    if (str[strlen(str) - 1] == ' ' && n) n--;
+    printf("%d", n);
+}
